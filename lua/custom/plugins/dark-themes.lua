3fd4800c2dd523ba01a8e654bd424b225f879716
multiplex55 <multiplex55@yahoo.com>
2025-04-22 20:30:58 -0400

    Theme and theme switcher

---
+++ lua/custom/plugins/dark-themes.lua
@@ -0,0 +1,21 @@
+return {
+  -- Already added
+  { 'shaunsingh/nord.nvim', lazy = true },
+  { 'navarasu/onedark.nvim', lazy = true },
+  { 'EdenEast/nightfox.nvim', lazy = true },
+  { 'rebelot/kanagawa.nvim', lazy = true },
+  { 'sainnhe/gruvbox-material', lazy = true },
+  { 'projekt0n/github-nvim-theme', lazy = true },
+  { 'marko-cerovac/material.nvim', lazy = true },
+  { 'rose-pine/neovim', name = 'rose-pine', lazy = true },
+
+  -- New dark-only themes
+  { 'nyoom-engineering/oxocarbon.nvim', lazy = true }, -- IBM Carbon-inspired dark
+  { 'bluz71/vim-nightfly-colors', name = 'nightfly', lazy = true },
+  { 'fenetikm/falcon', lazy = true }, -- Deep dark purple-blue
+  { 'yashguptaz/calvera-dark.nvim', lazy = true },
+  { 'kvrohit/mellow.nvim', lazy = true }, -- Calm dim purple
+  { 'ramojus/mellifluous.nvim', lazy = true }, -- Elegant, purply hues
+  { 'mcchrish/zenbones.nvim', dependencies = { 'rktjmp/lush.nvim' }, lazy = true },
+  { 'lunarvim/darkplus.nvim', lazy = true }, -- VS Code dark+
+}
--- lua/custom/utils.lua
+++ lua/custom/utils.lua
@@ -1,50 +1,91 @@
+-- Color switching
 local M = {}
 
 M.switch_colorscheme = function()
   local themes = {
+    -- Existing
     'catppuccin',
     'tokyonight',
     'tokyonight-night',
     'tokyonight-moon',
     'tokyonight-day',
+    'nord',
+    'onedark',
+    'carbonfox',
+    'duskfox',
+    'kanagawa',
+    'kanagawa-wave',
+    'kanagawa-dragon',
+    'gruvbox-material',
+    'github_dark',
+    'material',
+    'rose-pine',
+    'rose-pine-moon',
+    'oxocarbon',
+    'nightfly',
+    'falcon',
+    'calvera-dark',
+    'mellow',
+    'mellifluous',
+    'darkplus',
+    'zenbones',
   }
 
   local original = vim.g.colors_name
 
   require('telescope.pickers')
-    .new({}, {
+    .new({
+      layout_config = {
+        width = 0.35,
+        height = 0.6,
+        prompt_position = 'top',
+      },
+      layout_strategy = 'vertical',
+    }, {
       prompt_title = '🎨 Switch Colorscheme',
       finder = require('telescope.finders').new_table { results = themes },
       sorter = require('telescope.config').values.generic_sorter {},
-      attach_mappings = function(_, map)
+      attach_mappings = function(prompt_bufnr, map)
         local actions = require 'telescope.actions'
         local state = require 'telescope.actions.state'
 
         local preview = function()
           local entry = state.get_selected_entry()
           if entry then
-            vim.cmd.colorscheme(entry.value)
+            pcall(vim.cmd.colorscheme, entry.value)
           end
         end
 
-        map('i', '<CR>', function(bufnr)
-          preview()
-          actions.close(bufnr)
-        end)
+        -- Preview initial selection
+        vim.defer_fn(preview, 0)
 
+        -- Live preview on selection change
+        map('i', '<Down>', function()
+          actions.move_selection_next(prompt_bufnr)
+          preview()
+        end)
+        map('i', '<Up>', function()
+          actions.move_selection_previous(prompt_bufnr)
+          preview()
+        end)
         map('i', '<C-n>', function()
-          actions.move_selection_next()
+          actions.move_selection_next(prompt_bufnr)
           preview()
         end)
-
         map('i', '<C-p>', function()
-          actions.move_selection_previous()
+          actions.move_selection_previous(prompt_bufnr)
           preview()
         end)
 
+        -- Confirm selection
+        map('i', '<CR>', function()
+          actions.close(prompt_bufnr)
+        end)
+
+        -- Revert on cancel
         map('i', '<Esc>', function()
-          vim.cmd.colorscheme(original)
-          actions.close()
+          pcall(vim.cmd.colorscheme, original)
+          actions.close(prompt_bufnr)
         end)
 
         return true
@@ -52,5 +93,4 @@ M.switch_colorscheme = function()
     })
     :find()
 end
-
 return M
