24ef6a50a3685121e7d6a3ea3944650f4f3dc25c
multiplex55 <multiplex55@yahoo.com>
2025-05-06 18:51:25 -0400

    really terrible Mason change where the get install path was removed, see commit 7f265cd6ae56cecdd0aa50c8c73fc593b0604801

--- lua/custom/dap-config.lua
+++ lua/custom/dap-config.lua
@@ -1,8 +1,10 @@
 local dap = require 'dap'
 
 local mason_registry = require 'mason-registry'
-local codelldb = mason_registry.get_package 'codelldb'
-local extension_path = codelldb:get_install_path() .. '\\extension\\'
+
+-- Use environment variable to get install path
+local install_path = vim.fn.expand '$MASON' .. '\\packages\\codelldb'
+local extension_path = install_path .. '\\extension\\'
 local codelldb_path = extension_path .. 'adapter\\codelldb.exe'
 local liblldb_path = extension_path .. 'lldb\\bin\\liblldb.dll'
 
--- lua/custom/plugins/rustaceanvim.lua
+++ lua/custom/plugins/rustaceanvim.lua
@@ -1,24 +1,29 @@
-return {{ -- RUST
+return {
+  { -- RUST
     'mrcjkb/rustaceanvim',
     version = '^5', -- Recommended
     lazy = false, -- plugin is already lazy
     ['rust-analyzer'] = {
-        cargo = {
-            allFeatures = true
-        }
+      cargo = {
+        allFeatures = true,
+      },
     },
     config = function()
-        local mason_registry = require 'mason-registry'
-        local codelldb = mason_registry.get_package 'codelldb'
-        local extension_path = codelldb:get_install_path() .. '/extension/'
-        local codelldb_path = extension_path .. 'adapter/codelldb'
-        local liblldb_path = extension_path .. 'lldb/lib/liblldb.dylib'
-        local cfg = require 'rustaceanvim.config'
+      local mason_registry = require 'mason-registry'
+      local codelldb = mason_registry.get_package 'codelldb'
 
-        vim.g.rustaceanvim = {
-            dap = {
-                adapter = cfg.get_codelldb_adapter(codelldb_path, liblldb_path)
-            }
-        }
-    end
-}}
+      -- Use environment variable to get install path
+      local install_path = vim.fn.expand '$MASON' .. '\\packages\\codelldb'
+      local extension_path = install_path .. '\\extension\\'
+      local codelldb_path = extension_path .. 'adapter\\codelldb.exe'
+      local liblldb_path = extension_path .. 'lldb\\bin\\liblldb.dll'
+      local cfg = require 'rustaceanvim.config'
+
+      vim.g.rustaceanvim = {
+        dap = {
+          adapter = cfg.get_codelldb_adapter(codelldb_path, liblldb_path),
+        },
+      }
+    end,
+  },
+}
